478c3b4aa54071e428f3942c561d6c30bb37ad24
SofiaCADU <162449213+SofiaCADU@users.noreply.github.com>
2025-04-01 12:38:46 -0300

    Add files via upload

--- bola8.py
+++ bola8.py
@@ -5,17 +5,35 @@
 import random
 
 respuestas = ["✅ Si, definitivamante.", 
-         "⭐ Con toda certeza, que si.", 
-         "🔒 Sin lugar a duda.",
-         "🤔 Respuesta confusa, inténtalo de nuevo.",
-         "🕑 Preguntalo nuevamente más tarde.",
-         "🤫 Mejor no dercirte ahora.",
-         "✖️  Mis fuentes dicen que no.",
-         "🌧️ El panorama no es muy favorable.",
-         "🤷‍♂️ Muy dudoso."]
+              "⭐ Con toda certeza, que si.", 
+              "🔒 Sin lugar a duda.",
+              "🤔 Respuesta confusa, inténtalo de nuevo.",
+              "🕑 Preguntalo nuevamente más tarde.",
+              "🤫 Mejor no dercirte ahora.",
+              "✖️  Mis fuentes dicen que no.",
+              "🌧️ El panorama no es muy favorable.",
+              "🤷‍♂️ Muy dudoso."]
 
 pregunta = input('pregunta:')
 
-respuesta = random.choice(respuestas)
+respuesta = random.randint(1,8)
 
-print('respuesta:', respuesta)
+if respuesta == 1 :
+    print("respuesta:", "✅ Si, definitivamante.")
+elif respuesta == 2:
+    print("Respuesta:", "⭐ Con toda certeza, que si.")
+elif respuesta == 3:
+    print("Respuesta:", "🔒 Sin lugar a duda.")
+elif respuesta == 4:
+    print("Respuesta:", "🤔 Respuesta confusa, inténtalo de nuevo.")
+elif respuesta == 5:
+    print("Respuesta:", "🕑 Preguntalo nuevamente más tarde.")
+elif respuesta == 6:
+    print("Respuesta:", "🤫 Mejor no dercirte ahora.")
+elif respuesta == 7:
+    print("Respuesta:", "✖️  Mis fuentes dicen que no.")
+elif respuesta == 8:
+    print("Respuesta:", "🌧️ El panorama no es muy favorable.")
+else:
+    print("Respuesta:", "🤷‍♂️ Muy dudoso.")
+
