1c2f620a1d05c9e32edcc2ecbc77318330608449
SofiaCADU <162449213+SofiaCADU@users.noreply.github.com>
2025-03-31 22:18:35 -0300

    Add files via upload

---
+++ bola8.py
@@ -0,0 +1,19 @@
+# Sofía Cáceres
+# 31-03-2025
+# Tengo 8 peluches de rana
+
+import random
+
+pregunta = input('pregunta:')
+bola8 = ["✅ Si, definitivamante.", 
+         "⭐ Con toda certeza, que si.", 
+         "🔒 Sin lugar a duda."
+         "🤔 Respuesta confusa, inténtalo de nujevo."
+         "🕑 Preguntalo nuevamente más tarde."
+         "🤫 Mejor no dercirte ahora."
+         "✖️ Mis fuentes dicen que no."
+         "🌧️ El panorama no es muy favorable."
+         "🤷‍♂️ Muy dudoso."]
+
+if bola8 > 1:
+    print('respuesta:', respuesta[bola8])
